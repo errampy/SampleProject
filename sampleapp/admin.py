7500484106de2a64b486dc9dc977bbcc5e58efb5
bberrampy <ramlochan@bharathbrands.in>
2024-02-03 18:22:11 +0300

    added the models into admin pannel

--- sampleapp/admin.py
+++ sampleapp/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from sampleapp.models import *
 # Register your models here.
 
-admin.site.register(CompanyRegistration)
+class CompanyRegistrationAdmin(admin.ModelAdmin):
+    list_display=['company_id','company_name','company_description','established_at','created_at','updated_at']
+admin.site.register(CompanyRegistration,CompanyRegistrationAdmin)
+
+
+class DesignationAdmin(admin.ModelAdmin):
+    list_display=['desig_id','desig_name','created_at','updated_at']
+admin.site.register(Designation,DesignationAdmin)
+
+
+class EmployeeRegistrationAdmin(admin.ModelAdmin):
+    list_display=['emp_id','emp_name','dob','doj','address','created_at','updated_at']
+admin.site.register(EmployeeRegistration,EmployeeRegistrationAdmin)
 
